6cff5092c78f98c20879a65effc47a8fa1d505e7
MightyBoolik <bkuchaev@gmail.com>
2021-03-26 02:27:03 +0500

    Added singleResourceRequestTest

---
+++ src/test/java/tests/ReqresTests.java
@@ -0,0 +1,30 @@
+package tests;
+
+import io.qameta.allure.restassured.AllureRestAssured;
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.is;
+
+public class ReqresTests {
+    @BeforeAll
+    static void setup() {
+        RestAssured.filters(new AllureRestAssured());
+        RestAssured.baseURI = "https://reqres.in/";
+    }
+
+    @Test
+    void singleResourceRequestTest() {
+        given()
+                .when()
+                .get("/api/unknown/2")
+                .then()
+                .statusCode(200)
+                .log().body()
+                .body("support.url",
+                        is("https://reqres.in/#support-heading"));
+
+    }
+}
